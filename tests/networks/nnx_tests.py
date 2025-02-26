61eb43a6513d708b18f61f16a543e751adf7e230
willisma <nm3607@nyu.edu>
2025-02-26 23:43:11 +0000

    nnx tests

--- tests/networks/nnx_tests.py
+++ tests/networks/nnx_tests.py
@@ -17,7 +17,7 @@ class TestLinear(unittest.TestCase):
 
     def setUp(self):
         self.in_dim = 4
-        self.out_dim = 768
+        self.out_dim = 1152
         self.dtype = jnp.float32
         self.dshape = (4, 32, 32, 4)
 
@@ -82,7 +82,7 @@ class TestLinear(unittest.TestCase):
         for _ in range(self.sim_iter):
             nnx_y = nnx_forward(x)
         nnx_time = (time.time() - start_time) / self.sim_iter
-        print(f"========== NN time: {nn_time}, NNx time: {nnx_time} ==========")
+        print(f"========== Linear NN time: {nn_time}, NNx time: {nnx_time} ==========")
 
 
 class TestConv2D(unittest.TestCase):
@@ -90,7 +90,7 @@ class TestConv2D(unittest.TestCase):
 
     def setUp(self):
         self.in_dim = 4
-        self.out_channel = 768
+        self.out_channel = 1152
         self.dtype = jnp.float32
         self.dshape = (4, 32, 32, 4)
 
@@ -154,18 +154,18 @@ class TestConv2D(unittest.TestCase):
         for _ in range(self.sim_iter):
             nnx_y = nnx_forward(x)
         nnx_time = (time.time() - start_time) / self.sim_iter
-        print(f"========== NN time: {nn_time}, NNx time: {nnx_time} ==========")
+        print(f"========== Conv NN time: {nn_time}, NNx time: {nnx_time} ==========")
 
 
 class TestMHA(unittest.TestCase):
     """Test MHA module in nnx"""
 
     def setUp(self):
-        self.in_dim = 4
-        self.out_dim = 768
+        self.num_heads = 16
+        self.dim = 1152
         self.num_heads = 8
         self.dtype = jnp.float32
-        self.dshape = (4, 32, 32, 4)
+        self.dshape = (4, 256, 1152)
 
         self.nn_rng = jax.random.PRNGKey(0)
         self.nn_mha = nn.MultiHeadDotProductAttention(
@@ -178,11 +178,62 @@ class TestMHA(unittest.TestCase):
         )
 
         self.nnx_rng = nnx.Rngs(params=42)
-        self.nnx_mha = nnx.MHA(
-            self.in_dim, self.out_dim, num_heads=self.num_heads, dtype=self.dtype, rngs=self.nnx_rng
+        self.nnx_mha = nnx.MultiHeadAttention(
+            self.num_heads, self.dim, dtype=self.dtype, rngs=self.nnx_rng,
+            decode=False,
+            kernel_init=nn.initializers.lecun_uniform(),
+            bias_init=nn.initializers.zeros
         )
 
         self.data_rng = jax.random.PRNGKey(42)
+        self.sim_iter = 1_000
+    
+    def test_mha(self):
+        """Test MHA module."""
+        
+        nn_param = self.nn_param['params']
+
+        nn_param['query']['kernel'] = self.nnx_mha.query.kernel.value.copy()
+        nn_param['query']['bias'] = self.nnx_mha.query.bias.value.copy()
+
+        nn_param['key']['kernel'] = self.nnx_mha.key.kernel.value.copy()
+        nn_param['key']['bias'] = self.nnx_mha.key.bias.value.copy()
+
+        nn_param['value']['kernel'] = self.nnx_mha.value.kernel.value.copy()
+        nn_param['value']['bias'] = self.nnx_mha.value.bias.value.copy()
+        
+        nn_param['out']['kernel'] = nn.initializers.lecun_uniform()(
+            jax.random.fold_in(jax.random.PRNGKey(42), 3), (self.num_heads, self.dim // self.num_heads, self.dim)
+        )
+        nn_param['out']['bias'] = nn.initializers.zeros(
+            jax.random.fold_in(jax.random.PRNGKey(42), 3), (self.dim,)
+        )
+
+        nn_param['out']['kernel'] = self.nnx_mha.out.kernel.value.copy()
+        nn_param['out']['bias'] = self.nnx_mha.out.bias.value.copy()
+
+        # check equivalence in output
+        x = jax.random.normal(self.data_rng, self.dshape, dtype=self.dtype)
+        nn_y = self.nn_mha.apply({'params': nn_param}, x)
+        nnx_y = self.nnx_mha(x)
+
+        self.assertTrue(jnp.allclose(nn_y, nnx_y))
+
+        # benchmark compiled speed
+        nn_forward = jax.jit(self.nn_mha.apply)
+        nn_y = nn_forward({'params': nn_param}, x)
+        start_time = time.time()
+        for _ in range(self.sim_iter):
+            nn_y = nn_forward({'params': nn_param}, x)
+        nn_time = (time.time() - start_time) / self.sim_iter
+
+        nnx_forward = nnx.jit(self.nnx_mha)
+        nnx_y = nnx_forward(x)
+        start_time = time.time()
+        for _ in range(self.sim_iter):
+            nnx_y = nnx_forward(x)
+        nnx_time = (time.time() - start_time) / self.sim_iter
+        print(f"========== MHA NN time: {nn_time}, NNx time: {nnx_time} ==========")
 
 
 if __name__ == "__main__":
@@ -199,11 +250,6 @@ if __name__ == "__main__":
 
     key = rng1.params()
 
-    print(jnp.asarray(key), jax.random.fold_in(rng2, 0))  # 
-
-    nnx_Conv = nnx.Conv(
-        4, 768, kernel_size=(3, 3), dtype=jnp.float32, rngs=rng1
-    )
-    nnx.display(nnx_Conv)
+    print(jnp.asarray(key), jax.random.fold_in(rng2, 0))  # should be equal
 
     unittest.main()
