817b8da7fef16abc086f34e6d1a9028aa5c6f1cf
willisma <nm3607@nyu.edu>
2025-03-03 22:16:22 +0000

    add non-sharding ckpt support

--- tests/utils/checkpoint_tests.py
+++ tests/utils/checkpoint_tests.py
@@ -8,11 +8,11 @@ import jax
 import jax.numpy as jnp
 import flax
 from flax import nnx
+import numpy as np
 import optax
 import orbax.checkpoint as ocp
 
 # deps
-
 
 
 ckpt_dir = ocp.test_utils.erase_and_create_empty('/tmp/my-checkpoints/')
@@ -35,29 +35,142 @@ def loss_fn(model: nnx.Module, batch):
 def train_step(
     model: nnx.Module, optimizer: nnx.Optimizer, metrics: nnx.MultiMetric, batch
 ):
-    grad_fn = jax.value_and_grad(loss_fn)
+    grad_fn = nnx.value_and_grad(loss_fn)
     loss, grads = grad_fn(model, batch)
     metrics.update(loss=loss)
     optimizer.update(grads)
 
+
+class TestSaveAndRestore(unittest.TestCase):
+
+    def setUp(self):
+        self.model = TwoLayerMLP(128, nnx.Rngs(params=42))
+
+        self.learning_rate = 0.005
+        self.momentum = 0.9
+
+        self.optimizer = nnx.Optimizer(self.model, optax.adamw(self.learning_rate, self.momentum))
+        self.metrics = nnx.MultiMetric(loss=nnx.metrics.Average('loss'))
+
+    def test_ckptr(self):
+        """Test standard checkpointer in saving nnx model and opt state."""
+
+        # a short dummy training loop
+        data_rng = nnx.Rngs(0)
+        for _ in range(10):
+            x = jax.random.normal(data_rng(), (32, 128))
+            train_step(self.model, self.optimizer, self.metrics, x)
+
+        _, state = nnx.split(self.model)
+        checkpointer = ocp.Checkpointer(
+            ocp.CompositeCheckpointHandler()
+        )
+        _, opt_state = nnx.split(self.optimizer)
+
+        # optimizer is a simple wrapper around **both** model & opt state
+        jax.tree.map(lambda x, y: self.assertTrue(np.array_equal(x, y)), state, opt_state.model)
+
+        checkpointer.save(
+            ckpt_dir / 'state',
+            args=ocp.args.Composite(
+                state=ocp.args.StandardSave(state),
+                opt_state=ocp.args.StandardSave(opt_state)
+            )
+        )
+
+        # restore
+        abstract_model = nnx.eval_shape(
+            lambda: TwoLayerMLP(128, nnx.Rngs(params=42))
+        )
+        _, abstract_state = nnx.split(abstract_model)
+
+        abstract_opt_state = nnx.eval_shape(
+            lambda: nnx.Optimizer(
+                TwoLayerMLP(128, nnx.Rngs(params=42)), optax.adamw(self.learning_rate, self.momentum)
+            )
+        )
+        opt_graphdef, abstract_opt_state = nnx.split(abstract_opt_state)
+
+        state_restored = checkpointer.restore(
+            ckpt_dir / 'state',
+            args=ocp.args.Composite(
+                state=ocp.args.StandardRestore(abstract_state),
+                opt_state=ocp.args.StandardRestore(abstract_opt_state)
+            )
+        )
+
+        # this should not raise an error
+        jax.tree.map(
+            lambda x, y: self.assertTrue(np.array_equal(x, y)), state, state_restored.state
+        )
+        jax.tree.map(
+            lambda x, y: self.assertTrue(np.array_equal(x, y)), opt_state, state_restored.opt_state
+        )
+
+        # model = nnx.merge(graphdef, state_restored.state)
+        optimizer = nnx.merge(opt_graphdef, state_restored.opt_state)
+        model = optimizer.model  # <-- similar to torch, model must be referred by optimizer
+
+        # check training consistency
+        data_rng = nnx.Rngs(0)
+        xs = []
+        for _ in range(10):
+            x = jax.random.normal(data_rng(), (32, 128))
+            xs.append(x)
+            train_step(self.model, self.optimizer, self.metrics, x)
+        
+        for i in range(10):
+            x = xs[i]
+            train_step(model, optimizer, self.metrics, x)
+
+        jax.tree.map(
+            lambda x, y: self.assertTrue(np.array_equal(x, y)), nnx.split(self.model)[1], nnx.split(model)[1]
+        )
+        jax.tree.map(
+            lambda x, y: self.assertTrue(np.array_equal(x, y)), nnx.split(self.optimizer)[1].opt_state, nnx.split(optimizer)[1].opt_state
+        )
+
+    def test_ckptr_mngr(self):
+        """Test checkpoint manager in saving nnx model and opt state."""
+
+        # checkpoint manager is a simple wrapper around checkpointer, so we only need to test the high-level API
+        options = ocp.CheckpointManagerOptions(
+            save_interval_steps=16,  # this handles the control flow of how many steps to save
+            max_to_keep=4,
+            step_prefix='checkpoint',
+            keep_period=32,  # this keeps step % keep_period == 0; can be used as backup
+        )
+        # ckpt manager is async by default
+        ckpt_mngr = ocp.CheckpointManager(
+            ocp.test_utils.erase_and_create_empty('/tmp/mngr-checkpoints/'),
+            options=options
+        )
+        
+        rngs = nnx.Rngs(0)
+        for i in range(129):
+            train_step(self.model, self.optimizer, self.metrics, jax.random.normal(rngs(), (32, 128)))
+
+            ckpt_mngr.save(i, args=ocp.args.Composite(
+                state=ocp.args.StandardSave(nnx.split(self.model)[1]),
+                opt_state=ocp.args.StandardSave(nnx.split(self.optimizer)[1])
+            ))
+
+            ckpt_mngr.wait_until_finished()
+        
+        state_restored = ckpt_mngr.restore(128, args=ocp.args.Composite(
+            state=ocp.args.StandardRestore(nnx.split(self.model)[1]),
+            opt_state=ocp.args.StandardRestore(nnx.split(self.optimizer)[1])
+        ))
+
+        # state_restored = ckpt_mngr.restore(128)  <-- without specifying args this will return a nested dict
+
+        jax.tree.map(
+            lambda x, y: self.assertTrue(np.array_equal(x, y)), nnx.split(self.model)[1], state_restored.state
+        )
+        jax.tree.map(
+            lambda x, y: self.assertTrue(np.array_equal(x, y)), nnx.split(self.optimizer)[1], state_restored.opt_state
+        )
+
 if __name__ == "__main__":
 
-    model = TwoLayerMLP(128, nnx.Rngs(params=42))
-
-    learning_rate = 0.005
-    momentum = 0.9
-
-    optimizer = nnx.Optimizer(model, optax.adamw(learning_rate, momentum))
-    metrics = nnx.MultiMetric(
-        loss=nnx.metrics.Average('loss'),
-    )
-
-    train_steps = 1024
-
-    for i in range(train_steps):
-        batch = jax.random.normal(jax.random.PRNGKey(i), (32, 128))
-        train_step(model, optimizer, metrics, batch)
-
-        if i % 128 == 0:
-            for metric, value in metrics.compute().items():
-                print(f"{metric}: {value}")
+    unittest.main()
--- utils/checkpoint.py
+++ utils/checkpoint.py
@@ -3,11 +3,120 @@
 # built-in libs
 
 # external libs
+from absl import logging
+from etils import epath
 import jax
 import jax.numpy as jnp
 import flax
 from flax import nnx
-import orbax
+import orbax.checkpoint as ocp
 
 # deps
 
+
+# TODO: update the following functions to support sharding.
+def init_checkpoint_manager(
+    ckpt_dir: str,
+    *,
+    save_interval_steps: int,
+    max_to_keep: int,
+    keep_period: int,
+    step_prefix: str = 'checkpoint',
+    enable_async_checkpointing: bool = True,
+) -> ocp.CheckpointManager:
+    """Create a checkpoint manager for saving and restoring checkpoints during training."""
+
+    options = ocp.CheckpointManagerOptions(
+        save_interval_steps=save_interval_steps,  # this handles the control flow of how many steps to save
+        max_to_keep=max_to_keep, # this handles the control flow of how many checkpoints to keep
+        step_prefix=step_prefix,
+        keep_period=keep_period,  # this keeps step % keep_period == 0; can be used as backup
+        enable_async_checkpointing=enable_async_checkpointing
+    )
+    return ocp.CheckpointManager(ckpt_dir, options=options)
+
+
+def save_checkpoints(
+    ckpt_dir: str,
+    step: int,
+    optimizer_state: nnx.State,
+    ema_state: nnx.State,
+    *,
+    mngr: ocp.CheckpointManager | None = None,
+):
+    """Save checkpoints for model and optimizer state.
+    
+    Args:
+    - ckpt_dir: checkpoint directory.
+    - step: current step.
+    - optimizer_state: optimizer state. **Note** this is an analogy to Flax.TrainState,
+        which includes both opt_state & model_state
+    - ema_state: ema state.
+    """
+    
+    if mngr is None:
+        # persistent manager not supplied; use async checkpointer instead.
+        logging.warning('Checkpoint Manager not supplied; using default Checkpointer instead.')
+        ckptr = ocp.Checkpointer(ocp.CompositeCheckpointHandler())
+        ckptr.save(
+            epath.Path(ckpt_dir) / f'checkpoint_{step}',
+            args=ocp.args.Composite(
+                state=ocp.args.StandardSave(optimizer_state),
+                ema=ocp.args.StandardSave(ema_state)
+            )
+        )
+    else:
+        # persistent manager supplied; use it to manage saving logics
+        mngr.save(
+            step,
+            args=ocp.args.Composite(
+                state=ocp.args.StandardSave(optimizer_state),
+                ema_state=ocp.args.StandardSave(ema_state)
+            )
+        )
+
+
+def restore_checkpoints(
+    ckpt_dir: str,
+    step: int,
+    abstract_optimizer_state: nnx.State,
+    abstract_ema_state: nnx.State,
+    *,
+    mngr: ocp.CheckpointManager | None = None,
+) -> nnx.State:
+    """Restore checkpoints for model and optimizer state.
+    
+    Args:
+    - ckpt_dir: checkpoint directory.
+    - step: current step.
+    - optimizer_state: abstract optimizer state. **Note** this is an analogy to Flax.TrainState,
+        which includes both opt_state & model_state
+    - ema_state: abstract ema state.
+
+    Return:
+    - state: restored training state.
+    - ema_state: restored ema state.
+    """
+
+    if mngr is None:
+        # persistent manager not supplied; use async checkpointer instead.
+        logging.warning('Checkpoint Manager not supplied; using default Checkpointer instead.')
+        ckptr = ocp.Checkpointer(ocp.CompositeCheckpointHandler())
+        state_restored = ckptr.restore(
+            epath.Path(ckpt_dir) / f'checkpoint_{step}',
+            args=ocp.args.Composite(
+                state=ocp.args.StandardRestore(abstract_optimizer_state),
+                ema=ocp.args.StandardRestore(abstract_ema_state)
+            )
+        )
+    else:
+        # persistent manager supplied; use it to manage saving logics
+        state_restored = mngr.restore(
+            step,
+            args=ocp.args.Composite(
+                state=ocp.args.StandardRestore(abstract_optimizer_state),
+                ema_state=ocp.args.StandardRestore(abstract_ema_state)
+            )
+        )
+    
+    return state_restored
